8008399adfc9d2c63d3ffa1881d57ba93bba76d9
thicien <mugishathicien04@gmail.com>
2026-08-18 17:06:09 +0300

    feat: creating a new endpoints

---
+++ view.js
@@ -0,0 +1,19 @@
+import { createServer } from 'node:http'
+
+const server = createServer((req, res) => {
+  if (req.url === '/users' && req.method === 'GET') {
+    res.end('Users')
+  }
+})
+
+server.listen(3000)
+
+
+import { createServer} from 'node:http'
+const server = createServer((req, res) => {
+    if (req.url === '/users' && req.method === 'GET') {
+        res.end('Users')
+    } else (res.url === '/')
+})
+
+server.listen(3000)
